8194dae75caf683490ebca116bdc42371dd4baad
[dyad] <dbuzugbe@gmail.com>
2025-10-08 15:50:55 +0100

    [dyad] Updated 'Made with Dyad' text color and adjusted PartySidebar layout. - wrote 2 file(s)

--- src/components/made-with-dyad.tsx
+++ src/components/made-with-dyad.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export const MadeWithDyad = () => {
   return (
-    <div className="absolute bottom-4 right-4 text-sm text-white">
+    <div className="text-white">
       Made with Dyad. © 2025 dpb creative. All rights reserved.
     </div>
   );
