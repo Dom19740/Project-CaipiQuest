861c668402aefcd502e61b36da90c6fb3b9acf2e
[dyad] <dbuzugbe@gmail.com>
2025-12-14 15:15:37 +0700

    [dyad] Reduced font size of MadeWithDyad component - wrote 2 file(s) + extra files edited outside of Dyad

--- src/components/made-with-dyad.tsx
+++ src/components/made-with-dyad.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const MadeWithDyad = () => {
   return (
-    <div className="text-white text-center mt-4 px-4">
+    <div className="text-white text-center mt-4 px-4 text-xs">
       Made with Dyad. © 2025 <a href="https://dpbcreative.com/" target="_blank" rel="noopener noreferrer" className="underline hover:text-lime-200">dpbcreative.com</a>. All rights reserved.
     </div>
   );
