2cc9e0a4f0b59588ba070d6a20ce37b4242b19fa
Dom19740 <120669022+Dom19740@users.noreply.github.com>
2026-01-16 10:06:56 +0700

    Intro text updated on vscode

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -13,20 +13,19 @@ const Index = () => {
           className="mx-auto max-w-full h-auto w-80 sm:w-96 md:w-[400px] lg:w-[450px] xl:w-[500px] drop-shadow-lg mb-6"
         />
         <p className="text-base sm:text-lg text-gray-900 dark:text-gray-100 mb-8 max-w-prose mx-auto leading-relaxed">
-          Explore hundreds of delicious fruit combinations in this social, flavor-testing bingo game. 
+          CaipiQuest is a game to play in real life when you are with your friends at a bar, party or gathering.
+          <br /><br />
+          Create or Join a party, pick your fruits, check your bingo card and see who can complete their card first!
+          <br /><br />
+          Each time you drink a new combo of your fruits - mark your card.
+          <br /><br />
+          First person to get a line, diagonal or the entire card wins. Play for fun, points or bragging rights among your friends!
         </p>
         <img
           src="/welcome_grid.gif"
           alt="CaipiQuest Bingo Grid Example"
           className="mx-auto my-8 max-w-full h-auto rounded-lg shadow-lg border-2 border-lime-500 dark:border-lime-800"
         />        
-        <p className="text-base sm:text-lg text-gray-900 dark:text-gray-100 mb-8 max-w-prose mx-auto leading-relaxed">
-          Pick your favorite fruits to play.
-          <br /><br />
-          Each time you drink a new combo of your fruits - mark your card.
-          <br /><br />
-          First person to get a line, diagonal or the entire card wins. Play for fun, points or bragging rights among your friends!
-        </p>
         <Link to="/lobby">
           <Button className="w-full sm:w-auto px-10 py-5 text-lg bg-caipi hover:bg-caipi-hover text-white rounded-full shadow-lg transition-all duration-300 ease-in-out transform hover:scale-105 h-14">
             Play CaipiQuest
